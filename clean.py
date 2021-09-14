038f79c19314d2305155f3da48612a321a34c6f6
the-unreal <satyam.dey.23@gmail.com>
2021-09-14 20:27:37 +0000

    Added data.py

--- clean.py
+++ clean.py
@@ -4,35 +4,22 @@ from PIL import Image
 
 if __name__ == "__main__":
   path = '/content/dataset-iiit-pet'
-  count = 0
-  count_except = 0
-  count_try = 0
+  image_path = os.path.join(path, 'images')
+  mask_path = os.path.join(path, 'annotations/trimaps')
   x = []
-  for filename in os.listdir(os.path.join(path, 'images')):
+
+  for filename in os.listdir(image_path):
     if filename.endswith('.jpg'):
-      image_path = os.path.join(path, 'images')
       try:
         img = Image.open(os.path.join(image_path, filename))  # open the image file
         img.verify()  # verify that it is, in fact an image
-        count_try = count_try + 1
       except (IOError, SyntaxError) as e:
-        count_except = count_except + 1
-        """mask_path = os.path.join(path, 'annotations/trimaps')
-        print('Image: ', os.path.exists(os.path.join(image_path, filename)))
-        print('Mask: ', os.path.exists(os.path.join(mask_path, filename)))
-        """
+        x.append(filename.split('.')[0])
+        os.remove(os.path.join(image_path, filename))
     else:
-      print("inside else")
-      x.append(filename)
-      count = count + 1
-      
-      """image_path = os.path.join(path, 'images')"""
-      mask_path = os.path.join(path, 'annotations/trimaps')
-      """os.remove(os.path.join(image_path, filename))
-      os.remove(os.path.join(mask_path, filename))"""
-      print('Mask: ', os.path.join(mask_path, filename))
+      x.append(filename.split('.')[0])
+      os.remove(os.path.join(image_path, filename))
   
-  print(count)
-  print("try: ", count_try)
-  print("except: ", count_except)
-  print(x)
+  for mask_filename in os.listdir(mask_path):
+    if mask_filename.split('.')[0] in x:
+      os.remove(os.path.join(mask_path, mask_filename))
