01180aa88cec3bf918e4a0ae984b7b8465c1eab2
the-unreal <satyam.dey.23@gmail.com>
2021-09-14 17:29:32 +0000

    Added data.py

--- clean.py
+++ clean.py
@@ -4,15 +4,33 @@ from PIL import Image
 
 if __name__ == "__main__":
   path = '/content/data'
-  for filename in listdir(os.path.join(path, 'images')):
-    if filename.endswith('.JPG'):
+  count = 0
+  count_except = 0
+  count_try = 0
+  x = []
+  for filename in os.listdir(os.path.join(path, 'images')):
+    if filename.endswith('.jpg'):
       image_path = os.path.join(path, 'images')
       try:
         img = Image.open(os.path.join(image_path, filename))  # open the image file
         img.verify()  # verify that it is, in fact an image
+        count_try = count_try + 1
       except (IOError, SyntaxError) as e:
-        mask_path = os.path.join(path, 'annotations/trimaps')
-        print('Image: ', os.path.exists(mask_path, filename))
-        print('Mask: ', os.path.exists(mask_path, filename))
-        """os.remove(os.path.join(image_path, filename))
+        count_except = count_except + 1
+        """mask_path = os.path.join(path, 'annotations/trimaps')
+        print('Image: ', os.path.exists(os.path.join(image_path, filename)))
+        print('Mask: ', os.path.exists(os.path.join(mask_path, filename)))
+        ""os.remove(os.path.join(image_path, filename))
         os.remove(os.path.join(mask_path, filename))"""
+    else:
+      print("inside else")
+      x.append(filename)
+      count = count + 1
+      """image_path = os.path.join(path, 'images')
+      mask_path = os.path.join(path, 'annotations/trimaps')
+      print('Image: ', os.path.exists(os.path.join(image_path, filename)))
+      print('Mask: ', os.path.exists(os.path.join(mask_path, filename)))"""
+  print(count)
+  print("try: ", count_try)
+  print("except: ", count_except)
+  print(x)
